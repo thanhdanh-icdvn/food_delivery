d71611c1b2fe75843ef74cff257bff6e7b6d087c
thanhd <thanhdanh@icd-vn.com>
2023-01-17 16:44:50 +0700

    [FE] Update favicon.ico

--- frontend/types/twin.d.ts
+++ frontend/types/twin.d.ts
@@ -5,6 +5,7 @@ import { css as cssImport } from '@emotion/react';
 import { CSSInterpolation } from '@emotion/serialize';
 import styledImport from '@emotion/styled';
 
+/* Telling TypeScript that the module twin.macro has a styled and css import. */
 declare module 'twin.macro' {
   // The styled and css imports
   const styled: typeof styledImport;
@@ -13,15 +14,18 @@ declare module 'twin.macro' {
 
 declare module 'react' {
   // The css prop
+  /* Adding the tw and css props to the HTMLAttributes interface. */
   interface HTMLAttributes<T> extends DOMAttributes<T> {
     tw?: string;
     css?: CSSInterpolation;
   }
+  /* It's adding the css prop to the SVGProps interface. */
   // The inline svg css prop
   interface SVGProps<T> extends SVGProps<SVGSVGElement> {
     css?: CSSInterpolation;
   }
   // The tw and css prop
+  /* It's adding the css prop to the SVGProps interface. */
   interface DOMAttributes<T> {
     tw?: string;
     css?: CSSInterpolation;
