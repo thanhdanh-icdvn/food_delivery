b2e896f899a6f59518fb29590e8b10c6d0d8a7a6
thanhd <thanhdanh@icd-vn.com>
2023-02-09 15:16:09 +0700

    [BE] Remove wrong component and content-type

--- packages/backend/src/api/why-fast-food/controllers/why-fast-food.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * why-fast-food controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::why-fast-food.why-fast-food');
--- packages/backend/src/api/why-fast-food/services/why-fast-food.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * why-fast-food service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::why-fast-food.why-fast-food');
