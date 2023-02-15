19c182aaa00d6518b036e9f1f899a879728614f6
thanhd <thanhdanh@icd-vn.com>
2023-02-15 20:34:15 +0700

    [fix] Fix format frontend

--- packages/frontend/src/components/AccessDenied/index.tsx
+++ packages/frontend/src/components/AccessDenied/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { signIn } from 'next-auth/react';
+import Link from 'next/link';
 
 const AccessDenied = () => {
   return (
     <>
       <h1>You are not allowed to access</h1>
       <p>
-        <a
+        <Link
           href='/api/auth/signin'
           onClick={e => {
             e.preventDefault();
@@ -14,7 +15,7 @@ const AccessDenied = () => {
           }}
         >
           You must be signed in to view this page
-        </a>
+        </Link>
       </p>
     </>
   );
